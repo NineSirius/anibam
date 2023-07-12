948e987631af5d1a35c4fdf5cf6cb3177fb578d4
NineSirius <ruslanbulah3@gmail.com>
2023-07-13 02:06:00 +0600

    fix import bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './reducers/user.reducer'
-import globalSlice from './reducers/global.reducer'
 
 export const store = configureStore({
     reducer: userSlice,
