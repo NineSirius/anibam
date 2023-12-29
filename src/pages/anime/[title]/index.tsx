02d568c756387fc500ea5d663384c29cea927346
NineSirius <ruslanbulah3@gmail.com>
2023-12-30 00:31:35 +0600

    update announce button

--- src/pages/anime/[title]/index.tsx
+++ src/pages/anime/[title]/index.tsx
@@ -2,7 +2,6 @@ import { getAnilibriaSchedule, getAnilibriaTitle } from '@/api'
 import { TitlePage } from '@/containers/TitlePage'
 import { ScheduleT } from '@/containers/types/ScheduleT'
 import { TitleT } from '@/containers/types/TitleT'
-import { notFound } from 'next/navigation'
 import React from 'react'
 
 interface TitleProps {
@@ -17,19 +16,15 @@ const Title: React.FC<TitleProps> = ({ titleInfo, schedule }) => {
 export default Title
 
 export const getServerSideProps = async (context: any) => {
-    try {
-        const res = await getAnilibriaTitle(context.query.title)
-        const schedleResp = await getAnilibriaSchedule()
-        const data = {
-            ...res,
-            player: {
-                ...res.player,
-                list: Object.keys(res.player.list).map((key: string) => res.player.list[key]),
-            },
-        }
-
-        return { props: { titleInfo: data, schedule: schedleResp.data } }
-    } catch (error) {
-        return notFound()
+    const res = await getAnilibriaTitle(context.query.title)
+    const schedleResp = await getAnilibriaSchedule()
+    const data = {
+        ...res,
+        player: {
+            ...res.player,
+            list: Object.keys(res.player.list).map((key: string) => res.player.list[key]),
+        },
     }
+
+    return { props: { titleInfo: data, schedule: schedleResp.data } }
 }
