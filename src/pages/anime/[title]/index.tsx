b673701106cfde11ec8b1b8813284eec31770a2c
NineSirius <ruslanbulah3@gmail.com>
2024-02-06 17:52:10 +0600

    updated episodepage

--- src/pages/anime/[title]/index.tsx
+++ src/pages/anime/[title]/index.tsx
@@ -1,7 +1,4 @@
-import { getAnilibriaSchedule, getAnilibriaTitle } from '@/api'
 import { TitlePage } from '@/containers/TitlePage'
-import { ScheduleT } from '@/containers/types/ScheduleT'
-import { TitleT } from '@/containers/types/TitleT'
 import React from 'react'
 
 const Title = () => {
